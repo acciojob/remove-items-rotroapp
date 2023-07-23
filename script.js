707d418ea5f0c3fedb3d5a59d353d03d887a9387
Rajat Sharma <35336213+rotroapp@users.noreply.github.com>
2023-07-23 12:41:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-let id = document.querySelector('#colorSelect').value;
-let id2 = document.querySelector("#btn");
-let val = document.querySelector("display");
+let id = document.querySelector('#colorSelect');
+let id2 = document.querySelector("input");
+let val = document.querySelectorAll("option");
+let dis = document.querySelector(".display");
+// fn = () => {
+// 	// dis.innerHTML = val[1].value;
+// 	 val.filter((el) => {
+// 		 // dis.innerHTML = id.value;
+// 		return  id.value != el.value;
+// 	})
+// }
 
-
-id2.addEventListener( 'click', () => {
-	val.innerHTML = "bbbbn";
-})
+id2.addEventListener( "click",  ()=> {
+	      const selectedIndex = id.selectedIndex;
+        dis.innerHTML = selectedIndex; 
+      // Check if an option is selected
+      if (selectedIndex !== -1) {
+        // Remove the selected option from the dropdown list
+        id.removeChild(id.options[selectedIndex]);
+      }
+    }); 
